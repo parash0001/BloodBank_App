853a28d60bb36d91a811bb1ad3788b747c9e1fe0
Parash Mainali <mainaliparash26@gmail.com>
2025-06-20 21:15:47 +0545

    login local datasource code

--- lib/features/login/data/data_source/local_datasource/login_local_datasource.dart
+++ lib/features/login/data/data_source/local_datasource/login_local_datasource.dart
@@ -1,6 +1,6 @@
-import 'package:ghar_sewa/core/error/failure.dart';
-import 'package:ghar_sewa/core/network/hive_service.dart';
-import 'package:ghar_sewa/features/login/data/model/login_hive_model.dart';
+import 'package:bloodbank/core/error/failure.dart';
+import 'package:bloodbank/core/network/hive_service.dart';
+import 'package:bloodbank/features/login/data/model/login_hive_model.dart';
 
 abstract interface class ILoginLocalDataSource {
   Future<bool> checkLogin(LoginHiveModel login);
