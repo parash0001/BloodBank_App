a48cc8dae9330512a208fbc40f307dacc2db0516
Parash Mainali <mainaliparash26@gmail.com>
2025-06-20 21:32:41 +0545

    home view and some import fixes

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,71 +1,76 @@
+import 'package:bloodbank/features/home/presentation/view_model/home_state.dart';
+import 'package:bloodbank/features/home/presentation/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:student_management/core/common/snackbar/my_snackbar.dart';
-import 'package:student_management/features/home/presentation/view_model/home_state.dart';
-import 'package:student_management/features/home/presentation/view_model/home_view_model.dart';
+
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  final bool _isDarkTheme = false;
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => HomeViewModel(),
+      child: const HomeScaffold(),
+    );
+  }
+}
 
+class HomeScaffold extends StatefulWidget {
+  const HomeScaffold({super.key});
+
+  @override
+  State<HomeScaffold> createState() => _HomeScaffoldState();
+}
+
+class _HomeScaffoldState extends State<HomeScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text("Blood Bank"),
         centerTitle: true,
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () {
-              // Logout code
-              showMySnackBar(
-                context: context,
-                message: 'Logging out...',
-                color: Colors.red,
-              );
-
               context.read<HomeViewModel>().logout(context);
-            },
-          ),
-          Switch(
-            value: _isDarkTheme,
-            onChanged: (value) {
-              // Change theme
-              // setState(() {
-              //   _isDarkTheme = value;
-              // });
             },
           ),
         ],
       ),
-      // body: _views.elementAt(_selectedIndex),
       body: BlocBuilder<HomeViewModel, HomeState>(
         builder: (context, state) {
-          return state.views.elementAt(state.selectedIndex);
+          return state.views[state.selectedIndex];
         },
       ),
       bottomNavigationBar: BlocBuilder<HomeViewModel, HomeState>(
         builder: (context, state) {
           return BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
+            currentIndex: state.selectedIndex,
+            selectedItemColor: Colors.red,
+            unselectedItemColor: Colors.grey,
+            onTap: (index) {
+              context.read<HomeViewModel>().onTabTapped(index);
+            },
+            items: const [
               BottomNavigationBarItem(
                 icon: Icon(Icons.dashboard),
                 label: 'Dashboard',
               ),
-              BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Course'),
-              BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Batch'),
               BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: 'Account',
+                icon: Icon(Icons.people),
+                label: 'Donors',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bloodtype),
+                label: 'Requests',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
               ),
             ],
-            currentIndex: state.selectedIndex,
-            selectedItemColor: Colors.white,
-            onTap: (index) {
-              context.read<HomeViewModel>().onTabTapped(index);
-            },
           );
         },
       ),
--- lib/features/home/presentation/view_model/home_view_model.dart
+++ lib/features/home/presentation/view_model/home_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'home_state.dart';
 
